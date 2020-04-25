5f40aa67d691b95c31d8fe637a63d43fd691a421
Gmirmand <gmirmand@webqam.fr>
2020-04-25 23:13:07 +0200

    Add material-icons (& a default style.scss)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import './assets/sass/style.scss'
+
+import './assets/fonts/material-design-icons/material-icons.css'
+
 // Match Media
 import MQ from 'vue-match-media/src'
 
